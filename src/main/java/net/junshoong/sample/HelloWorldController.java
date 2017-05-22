9d54147d987fe836218b89176ace670b16f23ae7
Junshoong <junshoong@gmail.com>
2017-05-23 03:26:36 +0900

    change to annotations

--- src/main/java/net/junshoong/sample/HelloWorldController.java
+++ src/main/java/net/junshoong/sample/HelloWorldController.java
@@ -3,16 +3,16 @@ package net.junshoong.sample;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-public class HelloWorldController implements Controller {
+@Controller(value="sayHelloController")
+@RequestMapping("/saySomething")
+public class HelloWorldController {
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@RequestMapping("/sayhello")
+	public ModelAndView sayHello() {
 		Map<String, String> modelData = new HashMap<String, String>();
 		modelData.put("msg", "Hello World !!");
 		return new ModelAndView("helloworld", modelData);
