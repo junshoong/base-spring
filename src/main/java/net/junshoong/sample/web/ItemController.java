3dd8abb8a054147c56cca04ad5a786791b46afc3
Junshoong <junshoong@gmail.com>
2017-05-23 12:02:54 +0900

    add create method

--- src/main/java/net/junshoong/sample/web/ItemController.java
+++ src/main/java/net/junshoong/sample/web/ItemController.java
@@ -8,13 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.junshoong.sample.domain.ItemDetails;
 import net.junshoong.sample.service.ItemService;
 
 @Controller
-@RequestMapping("/todo")
+@RequestMapping("/item")
 public class ItemController {
 	
 	@Autowired
@@ -27,5 +28,34 @@ public class ItemController {
 		return new ModelAndView("itemList", modelData);
 	}
 	
+	@RequestMapping(params = "itemAction=create", method = RequestMethod.POST)
+	public ModelAndView createItem(@RequestParam Map<String, String> params) {
+		String content = params.get("content");
+		String level = params.get("level");
+		
+		Map<String, Object> errorData = new HashMap<String, Object>();
+		
+		if (content == null) {
+			errorData.put("error.content", "enter a String");
+		}
+		if (level == null) {
+			errorData.put("error.content", "enter a String");
+		}
+		
+		ItemDetails itemDetails = new ItemDetails();
+		itemDetails.setContent(content);
+		itemDetails.setLevel(level);
+		
+		if (errorData.size() > 0) {
+			errorData.put("itemDetail", itemDetails);
+			return new ModelAndView("itemList", errorData);
+		} else {
+			itemService.saveItem(itemDetails);
+			return new ModelAndView("redirect:/item/list");
+		}
+		
+
+	}
+
 
 }
--- src/main/webapp/WEB-INF/views/itemList.jsp
+++ src/main/webapp/WEB-INF/views/itemList.jsp
@@ -12,8 +12,20 @@
 <body>
 <div class="container">
 	<h1> To do List </h1>
-	<form name="itemList" method="POST"
-		action="${pageContext.request.contextPath}/">
+	<form class="form-inline" method="POST"
+		action="${pageContext.request.contextPath}/item?itemAction=create">
+		<div class="form-group">
+			<label for="itemContent">Content</label>
+			<input type="text" class="form-control" id="itemContent" name="content"
+				 placeholder="My To do" value="${requestScope.itemDetail.content}"/>
+		</div>
+		<div class="form-group">
+			<label for="itemLevel">Level</label>
+			<input type="text" class="form-control" id="itemLevel" name="level"
+				placeholder="Hard" value="${requestScope.itemDetail.level}"/>
+		</div>
+		<button type="submit" class="btn btn-default">Send invitation</button>
+	</form>
 		<table class="table table-hover">
 			<tr>
 				<th> id </th>
@@ -28,7 +40,6 @@
 			</tr>
 			</c:forEach>
 		</table>
-	</form>
 </div>
 </body>
 </html>
